10d44c39e2e5e13b9b4fb2a8af500edde1f726bf
tylerhackbart <t.hackbart@gmail.com>
2023-10-05 17:21:02 -0400

    Added tests

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,8 +5,8 @@ const config: StorybookConfig = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
+    // "@storybook/addon-onboarding",
+    // "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-webpack5",
---
+++ src/components/Grid/Grid.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Grid from "./Grid";
+
+describe("Grid", () => {
+  test("renders the Grid component", () => {
+    render(<Grid />);
+  });
+});

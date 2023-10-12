c8c0945cd31f6d9b653b9442bc0951d14f9431a4
Ajai Maxwel <axavierjames@postmedia.com>
2023-10-12 17:26:00 -0400

    Linting fix

--- src/types/index.ts
+++ src/types/index.ts
@@ -11,12 +11,33 @@ export interface GridProps {
     * The list of sections
     */
     sections: GridSectionProps[];
+    /**
+     * Section to display on extra small screens (<320px)
+     */
     xsSection?: GridSectionProps[];
+    /**
+     *  Section to display on small screens (<640px)
+     */
     smSection?: GridSectionProps[];
+    /**
+     *  Section to display on medium screens (<768px)
+     */
     mdSection?: GridSectionProps[];
+    /**
+     *  Section to display on large screens (<1024px)
+     */
     lgSection?: GridSectionProps[];
+    /**
+     *  Section to display on extra large screens (<1280px)
+     */
     xlSection?: GridSectionProps[];
+    /**
+     *  Section to display on double extra large screens (<1536px)
+     */
     xxlSection?: GridSectionProps[];
+    /**
+     *  Section to display on screens greater than 1536px
+     */
     xxxlSection?: GridSectionProps[];
 }
 
@@ -63,35 +84,29 @@ export interface GridSectionProps {
     subSection?: SubSection[]
     // TODO: Add `xAlign` and `yAlign`
     /**
-         * Visibility of the section on extra small screens (<320px)
-         */
+     * Visibility of the section on extra small screens (<320px)
+     */
     xsSectionDisplay?: Display;
-
     /**
      * Visibility of the section on small screens (<640px)
      */
     smSectionDisplay?: Display;
-
     /**
      * Visibility of the section on medium screens (<768px)
      */
     mdSectionDisplay?: Display;
-
     /**
      * Visibility of the section on large screens (<1024px)
      */
     lgSectionDisplay?: Display;
-
     /**
      * Visibility of the section on extra large screens (<1280px)
      */
     xlSectionDisplay?: Display;
-
     /**
      * Visibility of the section on double extra large screens (<1536px)
      */
     xxlSectionDisplay?: Display;
-
     /**
      * Visibility of the section on screens greater than 1536px
      */
